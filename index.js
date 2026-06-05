5f02a6941664aaf82c448acec4d36a6ebe80a216
ramonferva <ramonferv@gmail.com>
2026-06-05 15:49:03 -0400

    cambio de puesto en index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 const apiRouter = require('./routes/index');
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;  // ← Cambio aquí
 const app = express();
 
 app.use(express.urlencoded({extended:true}))
